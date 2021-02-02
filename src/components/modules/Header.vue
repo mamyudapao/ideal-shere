65a3f468320bc9d4f6defa3154da7c267351ed76
mamyudapao <nosikenit@gmail.com>
2021-02-02 12:04:23 +0900

    implemented navbar and installed font awesome

--- src/components/modules/Header.vue
+++ src/components/modules/Header.vue
@@ -1,40 +1,48 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light" style="background:#ffffff">
-      <div class="left-contets"></div>
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">idealShere</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse " id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-                <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/" class="nav-link active" aria-current="page">マイページ</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/" class="nav-link active" aria-current="page">通知</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/" class="nav-link active" aria-current="page">DM</router-link>
-            </li>
-          </ul>
+        <div class="left-contents">
+          <a class="navbar-brand" href="#">idealShere</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
         </div>
         
+        <div class="navbar-center" id="navbar-list">
+
+          <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                  <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
+              </li>
+              <li class="nav-item">
+                  <router-link to="/" class="nav-link active" aria-current="page">マイページ</router-link>
+              </li>
+              <li class="nav-item">
+                  <router-link to="/" class="nav-link active" aria-current="page">通知</router-link>
+              </li>
+              <li class="nav-item">
+                  <router-link to="/" class="nav-link active" aria-current="page">DM</router-link>
+              </li>
+            </ul>
+        </div>
+
+        </div>
+        
+        <i class="bi bi-plus-square"></i> 
+       <div class="navbar-right">
+       <font-awesome-icon icon="user-secret" />
       </div>
-      
+    </div>
     </nav>
   </div>
 </template>
@@ -42,4 +50,7 @@
 <script></script>
 
 <style>
+  #navbar-list {
+    padding-left: 10rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
